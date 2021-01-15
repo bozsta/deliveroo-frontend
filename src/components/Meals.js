8fac40ad5cabe36ac22128c9bffec5568cd3d1b4
Fabien <fabien.l@skilters.com>
2021-01-15 08:29:02 +0100

    update

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -4,7 +4,7 @@ const Meals = ({ meal }) => {
       <div className='card'>
         <div className='description'>
           <h3>{meal.title}</h3>
-          <p>{meal.description}</p>
+          {meal.description && <p>{meal.description}</p>}
           <div className='bottom-pane'>
             {meal.price && <span className='price'>{meal.price} €</span>}
             {meal.popular && <span className='popular'>Populaire</span>}
