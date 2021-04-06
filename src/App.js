fca192443029a8873bf7c00ad17731a742b6bf95
Fabien Luboz <lubozfabien@hotmail.com>
2021-04-06 23:10:09 +0200

    add fontawesome & update css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import './App.css';
-import Categorie from './components/Categorie'
-import RestoInfo from './components/RestoInfo'
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import logo from './assets/images/logo.png'
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import { useEffect, useState } from "react";
 import Loader from "react-loader-spinner";
-import Panier from './components/Panier'
+import axios from "axios";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import logo from "./assets/images/logo.png";
+import Categorie from "./components/Categorie";
+import RestoInfo from "./components/RestoInfo";
+import Panier from "./components/Panier";
+
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import "./App.css";
+
+library.add(faStar);
 
 function App() {
-  const [restoInfo, setRestoInfo] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
-  const [products, setProducts] = useState([])
+  const [restoInfo, setRestoInfo] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
 
   const fetchData = async () => {
-    const result = await axios.get('https://deliveroo-backend-pegasus.herokuapp.com/')
-    setRestoInfo(result.data)
-    setIsLoading(false)
-  }
+    const result = await axios.get(
+      "https://deliveroo-backend-pegasus.herokuapp.com/"
+    );
+    setRestoInfo(result.data);
+    setIsLoading(false);
+  };
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const handleAddArticle = (e) => {
-    let  product = JSON.parse(e.currentTarget.dataset.value)
-    const newProducts = [...products]
+    let product = JSON.parse(e.currentTarget.dataset.value);
+    const newProducts = [...products];
     let isFound = false;
 
     for (let i = 0; i < newProducts.length; i++) {
@@ -38,64 +46,75 @@ function App() {
       product.quantity = 1;
       newProducts.push(product);
     }
-    setProducts(newProducts)
-  }
+    setProducts(newProducts);
+  };
 
   const handleCountAdd = (id) => {
-    const newProducts = [...products]
+    const newProducts = [...products];
     for (let i = 0; i < newProducts.length; i++) {
       if (newProducts[i].id === id) {
         newProducts[i].quantity = newProducts[i].quantity + 1;
         break;
       }
     }
-    setProducts(newProducts)
-  }
+    setProducts(newProducts);
+  };
 
   const handleCountRemove = (id) => {
-    const newProducts = [...products]
-    console.log('newProducts', newProducts)
+    const newProducts = [...products];
+    console.log("newProducts", newProducts);
     for (let i = 0; i < newProducts.length; i++) {
       if (newProducts[i].id === id) {
         if (newProducts[i].quantity > 1) {
           newProducts[i].quantity = newProducts[i].quantity - 1;
         } else {
-          newProducts.splice(i, 1)
-        }    
+          newProducts.splice(i, 1);
+        }
         break;
       }
     }
-    setProducts(newProducts)
-  }
+    setProducts(newProducts);
+  };
 
   return (
     <div className="App">
       <header>
         <div className="container">
-          <div className="logo" ><img src={logo} alt="logo" /></div>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
         </div>
       </header>
       {!isLoading && <RestoInfo data={restoInfo.restaurant} />}
-      <div className="content" >
+      <div className="content">
         <div className="categories">
-          {!isLoading && restoInfo.categories.map((categorie) => {
-            return <Categorie key={categorie.name} name={categorie.name} meals={categorie.meals} addArticle={handleAddArticle}/>
-          })}
-          
+          {!isLoading &&
+            restoInfo.categories.map((categorie) => {
+              return (
+                <Categorie
+                  key={categorie.name}
+                  name={categorie.name}
+                  meals={categorie.meals}
+                  addArticle={handleAddArticle}
+                />
+              );
+            })}
         </div>
         <div className="leftSide">
-        {!isLoading && <Panier data={products} handleAdd={handleCountAdd} handleRemove={handleCountRemove} />}
-
+          {!isLoading && (
+            <Panier
+              data={products}
+              handleAdd={handleCountAdd}
+              handleRemove={handleCountRemove}
+            />
+          )}
         </div>
       </div>
-      {isLoading && <div className='overlay-loader'>
-        <Loader
-          type="Circles"
-          color="#FFFFFF"
-          height={100}
-          width={100}
-        />
-      </div>}
+      {isLoading && (
+        <div className="overlay-loader">
+          <Loader type="Circles" color="#FFFFFF" height={100} width={100} />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,20 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Meals = ({ meal, addtToCart }) => {
-  const value = { id: meal.id, title: meal.title, price: meal.price}
+  const value = { id: meal.id, title: meal.title, price: meal.price };
   return (
-    <div className='meal'>
-      <div className='card' onClick={addtToCart} data-value={JSON.stringify(value)}>
-        <div className='description'>
+    <div className="meal">
+      <div
+        className="card"
+        onClick={addtToCart}
+        data-value={JSON.stringify(value)}
+      >
+        <div className="description">
           <h3>{meal.title}</h3>
           {meal.description && <p>{meal.description}</p>}
-          <div className='bottom-pane'>
-            {meal.price && <span className='price'>{meal.price} €</span>}
-            {meal.popular && <span className='popular'>Populaire</span>}
+          <div className="bottom-pane">
+            {meal.price && <span className="price">{meal.price} €</span>}
+            {meal.popular && (
+              <span className="popular">
+                <FontAwesomeIcon icon="star" /> Populaire
+              </span>
+            )}
           </div>
         </div>
-        {meal.picture && <div className='picture'><img src={meal.picture} alt={meal.title} /></div>}
+        {meal.picture && (
+          <div className="picture">
+            <img src={meal.picture} alt={meal.title} />
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Meals
+export default Meals;
